e188dc7219cb7bbee26bd9dbb84ab2ed219de786
FS_Project <77166195+FS-Project@users.noreply.github.com>
2021-02-17 00:15:49 +0700

    typo

--- ferbot/modules/no_sql/disable_db.py
+++ ferbot/modules/no_sql/disable_db.py
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Group disabled commands database."""
 
-from ferubot.modules.no_sql import get_collection
+from ferbot.modules.no_sql import get_collection
 
 DISABLED_COMMANDS = get_collection("DISABLED_COMMANDS")
 
